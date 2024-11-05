14798c3ad44a94cc576f9387c31123c280824e8d
dittethorgaard <eaa23dith@students.eaaa.dk>
2024-11-05 10:40:27 +0100

    send

--- js.js
+++ js.js
@@ -4,63 +4,63 @@ const planets = [
         name: 'SOLEN',
         title: 'Stjernen i Solsystemet',
         description: 'Solen er en gul dværg (type G2) og udgør centrum af vores solsystem, hvor den indeholder omkring 99,86% af den samlede masse. Dens enorme gravitation holder de otte planeter og mange andre himmellegemer i bane omkring sig.',
-        image: 'path/to/sun.jpg'
+        image: 'path/to/sun.webp',
         orbitPosition: 18
     },
     {
         name: 'MERKUR',
         title: 'Nærmest Solen',
         description: 'Merkur er den planet, der ligger tættest på Solen, med en gennemsnitlig afstand på cirka 57,91 millioner km. Ekstrem temperatur: Den har ekstremt varierende temperaturer, der kan nå op til 430 grader Celsius om dagen og falde til -180 grader Celsius om natten.',
-        image: '???'
+        image: 'path/to/mercury.webp',
         orbitPosition: 36
     },
     {
         name: 'VENUS',
         title: 'Lignende størrelse som Jorden',
         description: 'Venus er ofte kaldt "Jordens tvilling" på grund af sin lignende størrelse og sammensætning, men dens atmosfære er meget forskelligartet. Tyk atmosfære: Atmosfæren består primært af kuldioxid og svovlsyre, hvilket skaber en ekstrem drivhuseffekt og gør overfladen til den varmeste i solsystemet.',
-        image: '???'
+        image: 'path/to/venus.webp',
         orbitPosition: 54
     },
     {
         name: 'JORDEN',
         title: 'Livets hjemsted',
         description: 'Jorden er den eneste planet kendt for at huse liv, med et varieret klima og store mængder vand. Aktiv pladetektonik: Den har en aktiv pladetektonik, hvilket betyder, at dens overflade konstant ændrer sig gennem geologiske processer.',
-        image: '???'
+        image: 'path/to/earth.webp',
         orbitPosition: 72
     },
     {
         name: 'MARS',
         title: 'Rød plante',
         description: 'Mars kaldes ofte den "røde planet" på grund af jernoxid (rust) på dens overflade, som giver den en rødlig farve. Mulighed for liv: Mars har været genstand for stor interesse i søgen efter liv, da der er fundet spor af vand i form af is og tidligere flydende vand.',
-        image: '???'
+        image: 'path/to/mars.webp',
         orbitPosition: 90
     },
     {
         name: 'JUPITER',
         title: 'Største planet',
         description: 'Jupiter er den største planet i solsystemet og har en diameter på omkring 139.822 km. Mange måner: Den har mere end 79 kendte måner, herunder de fire store måner kaldet Galileiske måner: Io, Europa, Ganymedes og Callisto.',
-        image: '???'
+        image: 'path/to/jupiter.webp',
         orbitPosition: 108
     },
     {
         name: 'SATURN',
         title: 'Ringe system',
         description: 'Saturn er berømt for sine imponerende ringsystemer, der består af ispartikler og små sten. Gasplanet: Som en gasplanet har Saturn ikke en fast overflade; dens atmosfære er primært sammensat af hydrogen og helium.',
-        image: '???'
+        image: 'path/to/saturn.webp',
         orbitPosition: 126
     },
     {
         name: 'URANUS',
         title: 'Skæv rotation',
         description: 'Uranus roterer på sin side med en aksel hældning på omkring 98 grader, hvilket gør dens rotationsakse næsten vandret. Koldest planet: Den har også den laveste temperatur blandt planeterne i solsystemet, med minimumstemperaturer omkring -224 grader Celsius.',
-        image: '???'
+        image: 'path/to/uranus.webp',
         orbitPosition: 144
     },
     {
         name: 'NEPTUN',
         title: 'Fjerntliggende planet',
         description: 'Neptun er den mest fjerntliggende planet i solsystemet og ligger cirka 4,5 milliarder km fra Solen. Vejrfænomener: Den er kendt for sine kraftige vinde og storme, herunder den berømte "Great Dark Spot," som minder om Jordens storme.',
-        image: '???'
+        image: 'path/to/neptun.webp',
         orbitPosition: 162
     },
 ];
